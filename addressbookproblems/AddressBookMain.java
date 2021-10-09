c3be278057273c369dcc13cf320b89e8d58bbb01
9908229348 <kalavaisravanthi98@gmail.com>
2021-10-09 12:27:23 +0530

    Refactored: Using Switch case to edit Particular detail

--- addressbookproblems/AddressBookMain.java
+++ addressbookproblems/AddressBookMain.java
@@ -34,42 +34,44 @@ public class AddressBookMain {
 		user.print(contactStore.getContactList());
 
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter choice 1.Add the new contact\n 2.Edit Existing contact 3.Remove the contact");
-		int choice = sc.nextInt();
-		switch (choice) {
-		case 1:
-			System.out.println("Enter how many persons you want to add");
-			int n = sc.nextInt();
-			for (int i = 1; i <= n; i++) {
+		boolean check = true;
+		while(check == true) {
+			System.out.println("Enter choice 1.Add the new contact\n 2.Edit Existing contact 3.Remove the contact");
+			int choice = sc.nextInt();
+			switch (choice) {
+			case 1:
 				AddressBook contact = new AddressBook();
 				user.addContact(contact);
 				contactStore.add(contact);
 				System.out.println("Contact List after add");
 				user.print(contactStore.getContactList());
+				break;
+			case 2:
+				Scanner s = new Scanner(System.in);
+				System.out.println("Enter name of contact you want to edit");
+				String name = s.nextLine();
+				if (contactOne.getFirstName().equalsIgnoreCase(name) == true)
+					user.edit(contactOne);
+				else if (contactTwo.getFirstName().equalsIgnoreCase(name) == true)
+					user.edit(contactTwo);
+				System.out.println("Contact List after edit");
+				user.print(contactStore.getContactList());
+				break;
+			case 3:
+				Scanner scanner = new Scanner(System.in);
+				System.out.println("Enter name of person you want to delete");
+				String contactName = scanner.nextLine();
+				if (contactOne.getFirstName().equalsIgnoreCase(contactName) == true)
+					contactStore.remove(contactOne);
+				else if (contactTwo.getFirstName().equalsIgnoreCase(contactName) == true)
+					contactStore.remove(contactTwo);
+				System.out.println("Contact List after deletion");
+				user.print(contactStore.getContactList());
+				break;
+			default :
+				check = false;
+				System.out.println("Invalid choice and Exited ");
 			}
-			break;
-		case 2:
-			Scanner s = new Scanner(System.in);
-			System.out.println("Enter name of contact you want to edit");
-			String name = s.nextLine();
-			if (contactOne.getFirstName().equalsIgnoreCase(name) == true)
-				user.edit(contactOne);
-			else if (contactTwo.getFirstName().equalsIgnoreCase(name) == true)
-				user.edit(contactTwo);
-			System.out.println("Contact List after edit");
-			user.print(contactStore.getContactList());
-			break;
-		case 3:
-			Scanner scanner = new Scanner(System.in);
-			System.out.println("Enter name of person you want to delete");
-			String contactName = scanner.nextLine();
-			if (contactOne.getFirstName().equalsIgnoreCase(contactName) == true)
-				contactStore.remove(contactOne);
-			else if (contactTwo.getFirstName().equalsIgnoreCase(contactName) == true)
-				contactStore.remove(contactTwo);
-			System.out.println("Contact List after deletion");
-			user.print(contactStore.getContactList());
-			break;
 		}
 	}
 }
--- addressbookproblems/UserInterface.java
+++ addressbookproblems/UserInterface.java
@@ -12,21 +12,71 @@ public class UserInterface {
 	}
 
 	void edit(AddressBook contact) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Edit the details firstName,lastName,address,city,state,zip,phoneNumber,email respectively");
-		contact.setFirstName(sc.nextLine());
-		contact.setLastName(sc.nextLine());
-		contact.setAddress(sc.nextLine());
-		contact.setCity(sc.nextLine());
-		contact.setState(sc.nextLine());
-		contact.setZip(sc.nextLine());
-		contact.setPhoneNumber(sc.nextLine());
-		contact.setEmail(sc.nextLine());
+		boolean check = true;
+		while (check == true) {
+			System.out.println("enter 1.firstName 2.lastName 3.address 4.city 5.state 6.zip 7.phonenumber 8.email to edit");
+			System.out.println("Enter number greater than 8 to Exit");
+			Scanner sc = new Scanner(System.in);
+			int choice = sc.nextInt();
+			switch (choice) {
+			case 1:
+				System.out.println("Enter your new firstName :");
+				Scanner sca = new Scanner(System.in);
+				String name = sca.nextLine();
+				contact.setFirstName(name);
+				break;
+			case 2:
+				System.out.println("Enter your new lastName : ");
+				Scanner scan = new Scanner(System.in);
+				String lastName = scan.nextLine();
+				contact.setLastName(lastName);
+				break;
+			case 3:
+				System.out.println("Enter your new address :");
+				Scanner scann = new Scanner(System.in);
+				String address = scann.nextLine();
+				contact.setAddress(address);
+				break;
+			case 4:
+				System.out.println("Enter your new city : ");
+				Scanner scanne = new Scanner(System.in);
+				String city = scanne.nextLine();
+				contact.setCity(city);
+				break;
+			case 5:
+				System.out.println("Enter your new state :");
+				Scanner s = new Scanner(System.in);
+				String state = s.nextLine();
+				contact.setCity(state);
+				break;
+			case 6:
+				System.out.println("Enter your new zip : ");
+				Scanner scanner = new Scanner(System.in);
+				String zip = scanner.nextLine();
+				contact.setZip(zip);
+				break;
+			case 7:
+				System.out.println("Enter your new phoneNumber :");
+				Scanner scanner1 = new Scanner(System.in);
+				String phoneNumber = scanner1.nextLine();
+				contact.setPhoneNumber(phoneNumber);
+				break;
+			case 8:
+				System.out.println("Enter your new email : ");
+				Scanner scanner2 = new Scanner(System.in);
+				String email = scanner2.nextLine();
+				contact.setEmail(email);
+				break;
+			default:
+				check = false;
+			}
+		}
 	}
 
 	void addContact(AddressBook contactThree) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter details of new person firstName,lastName,address,city,state,zip,phoneNumber,Email respectively");
+		System.out.println(
+				"Enter details of new person firstName,lastName,address,city,state,zip,phoneNumber,Email respectively");
 		contactThree.setFirstName(sc.nextLine());
 		contactThree.setLastName(sc.nextLine());
 		contactThree.setAddress(sc.nextLine());
